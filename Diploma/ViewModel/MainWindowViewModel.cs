134fc27521a84c0a7e4addd67a809974d18af499
RomanAstra <roman_muratov_astu@mail.ru>
2018-05-16 19:40:19 +0400

    Добавлен фреймворк для работы с паттерном MVVM. Добавлена тестовая команда. Исправлены мелкие баги

--- Diploma/ViewModel/MainWindowViewModel.cs
+++ Diploma/ViewModel/MainWindowViewModel.cs
@@ -1,7 +1,30 @@
-﻿namespace Diploma.ViewModel
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Diploma.Infrastructure;
+
+namespace Diploma.ViewModel
 {
 	public class MainWindowViewModel : ViewModelBase
 	{
+		private RelayCommand _testCommand;
+		public ICommand Test => _testCommand ?? (_testCommand =
+			                                new RelayCommand(ExecuteCommand,
+				                                CanExecuteCommand));
+
+		public void ExecuteCommand(object parameter)
+		{
+			try
+			{
+				MessageBox.Show("Test");
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show(e.Message);
+			}
+		}
+		public bool CanExecuteCommand(object parameter) => true;
+
 		protected override void OnDispose()
 		{
 			// Очистка ресурсов
