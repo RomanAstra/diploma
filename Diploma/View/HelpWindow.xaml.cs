ca0d582c986b813ea94065d375d63b29a909b1f4
RomanAstra <roman_muratov_astu@mail.ru>
2018-05-28 21:58:07 +0400

    Переделал вывод теории

--- Diploma/View/HelpWindow.xaml.cs
+++ Diploma/View/HelpWindow.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Markup;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Xml;
-using System.Xml.Linq;
 
 namespace Diploma.View
 {
@@ -23,93 +13,87 @@ namespace Diploma.View
     /// </summary>
     public partial class HelpWindow : Window
     {
-        private XElement xtv;
-        private XElement node;
-        private XElement attribute;
-
         public HelpWindow()
         {
             InitializeComponent();
 
-            string uri = @"pack://application:,,,/Help/XMLHelp.xml";
-            TreeView tvOld = Root.Children.OfType<TreeView>().FirstOrDefault();
-            if (tvOld != null)
-            {
-                Root.Children.Remove(tvOld);
-            }
-            ReadFiles();
-            XElement target = GetXElement(uri);
+	        Theory test1 = new Theory
+	        {
+		        Name = "ГОСТ",
+		        Specificity = new List<Specificity>
+		        {
+			        new Specificity
+			        {
+				        Name = "ГОСТ 25192-2012 Бетоны – Классификация и общие технические требования",
+				        Description = "http://www.gvozdem.ru/stroim-dom/kalkulyatory/calc_beton/file/25192-2012-gost.pdf"
+					},
 
-            foreach (XElement el in target.Elements())
-            {
-                XElement xaml = new XElement(node);
-                Process(el, xaml);
-                xtv.Add(xaml);
-            }
-            string tvString = xtv.ToString();
-            ParserContext context = new ParserContext();
-            context.XmlnsDictionary.Add("", "http://schemas.microsoft.com/winfx/2006/xaml/presentation");
-            context.XmlnsDictionary.Add("x", "http://schemas.microsoft.com/winfx/2006/xaml");
-            TreeView tv = (TreeView)XamlReader.Parse(tvString, context);
-            Root.Children.Add(tv);
-        }
+			        new Specificity
+					{
+				        Name = "ГОСТ 26633-91 Бетоны тяжелые и мелкозернистые",
+				        Description = "http://www.gvozdem.ru/stroim-dom/kalkulyatory/calc_beton/file/26633-91-gost.pdf"
+					},
 
-        private void ReadFiles()
-        {
-            string filename = System.AppDomain.CurrentDomain.BaseDirectory;
+			        new Specificity
+			        {
+				        Name = "ГОСТ 7473-2010 Смеси бетонные",
+				        Description = "http://www.gvozdem.ru/stroim-dom/kalkulyatory/calc_beton/file/7473-2010-gost.pdf"
+					}
+				}
+	        };
 
-            xtv = GetXElement(@"pack://application:,,,/Help/treeview.txt");
-            node = GetXElement(@"pack://application:,,,/Help/node.txt");
-            attribute = GetXElement(@"pack://application:,,,/Help/attribute.txt");
-        }
+	        Theory test2 = new Theory
+	        {
+		        Name = "СНИП",
+		        Specificity = new List<Specificity>
+		        {
+			        new Specificity
+			        {
+				        Name = "СНиП 82-02-95 Нормы расхода цемента при изготовление железобетонных изделий",
+				        Description = "http://www.gvozdem.ru/stroim-dom/kalkulyatory/calc_beton/file/82-02-95-snip.pdf"
+					}
+		        }
+			};
+			Theory[] test = new Theory[] { test1, test2 };
+			DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(Theory[]));
 
-        private XElement GetXElement(string uri)
-        {
-            XDocument xmlDoc = new XDocument();
-            var iob = new Uri(uri, UriKind.Absolute);
-            var xmltxt = Application.GetContentStream(iob);
-            string elfull = new StreamReader(xmltxt.Stream).ReadToEnd();
-            xmlDoc = XDocument.Parse(elfull);
-            return xmlDoc.Root;
-        }
+	        using (FileStream fs = new FileStream("Test.json", FileMode.OpenOrCreate))
+	        {
+		        jsonFormatter.WriteObject(fs, test);
+	        }
 
-        private void Process(XElement xml, XElement tv)
-        {
-            SetNameValue(tv, xml.Name.ToString(), "");
-            if (xml.HasAttributes)
-            {
-                foreach (XAttribute attr in xml.Attributes())
-                {
-                    XElement xa = new XElement(attribute);
-                    SetNameValue(xa, attr.Name.ToString(), attr.Value.ToString());
-                    tv.Add(xa);
-                }
-            }
-            if (xml.HasElements)
-            {
-                // Not a leaf node - process children
-                foreach (var childXml in xml.Elements())
-                {
-                    XElement child = new XElement(node);
-                    Process(childXml, child);
-                    tv.Add(child);
-                }
-            }
-            else
-            {
-                // Leaf node 
-                SetNameValue(tv, xml.Name.ToString(), xml.Value.ToString());
-            }
-        }
+	        using (FileStream fs = new FileStream("Test.json", FileMode.OpenOrCreate))
+	        {
+		        Theory[] newtest = (Theory[])jsonFormatter.ReadObject(fs);
 
-        private void SetNameValue(XElement xaml, string name, string value)
-        {
-            var nameEl = xaml.Descendants("TextBlock")
-                            .Single();
-            nameEl.SetAttributeValue("Text", name);
-            var valEl = xaml.Descendants("Label")
-                           .Single();
-            valEl.SetAttributeValue("Content", value);
+		        foreach (Theory p in newtest)
+				{
+					TreeViewItem item = new TreeViewItem {Header = p.Name};
+
+					foreach (var specificity in p.Specificity)
+					{
+						TreeViewItem item2 = new TreeViewItem { Header = specificity.Name };
+						item2.Items.Add(specificity.Description);
+						item.Items.Add(item2);
+					}
+					Root.Items.Add(item);
+				}
+	        }
         }
     }
+
+	[DataContract]
+	public class Theory
+	{
+		[DataMember]
+		public string Name { get; set; }
+		[DataMember]
+		public List<Specificity> Specificity { get; set; }
+	}
+
+	public class Specificity
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
 }
